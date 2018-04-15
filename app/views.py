0729f09df0dc07c03db53e9149c2f11d4164bf38
colataTT <zxy6320115@sina.com>
2018-04-15 23:59:43 +0800

    返回一个html标签 添加教程网址

--- app/views.py
+++ app/views.py
@@ -1,4 +1,16 @@
 from app import app
+
+@app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    user = { 'nickname': 'Miguel'}
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
